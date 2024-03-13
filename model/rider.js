78f7080ace52efc7303a275105bce44886e9c36c
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-03-13 16:41:23 +0500

    Edit the rider

--- model/rider.js
+++ model/rider.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const riderSchema = new mongoose.model({
+const riderSchema = new mongoose.Schema({
     user_account: {
         type: String
     },
     user_account_password: {
         type: String
     }
-},
-{
+},{
     timestamps: true
-});
+}
+);
 module.exports = mongoose.model("rider",riderSchema)
